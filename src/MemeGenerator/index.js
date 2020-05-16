094f40b26d86c8d6eb504ab6b02db049071ed4ed
heliorrfreitas <heliorui.ramosfreitas@gus.pittstate.edu>
2020-05-15 22:42:56 -0500

    Transform MemeGenerator into a functional component. Add useState. Add useEffect in the place of componentDidMount

--- src/MemeGenerator/index.js
+++ src/MemeGenerator/index.js
@@ -1,43 +1,56 @@
-import React, {Component} from "react"
-import MemeComponent from "./MemeComponent"
+import React, {useState, useEffect} from "react"
+import "./style.css"
 
-class MemeGenerator extends Component {
-    state = {
-            topText: "",
-            bottomText: "",
-            randomImage: "http://i.imgflip.com/1bij.jpg",
-            allMemeImages: []
-        }
-    
+function MemeGenerator() {
+    const [topText, setTopText] = useState("")
+    const [bottomText, setBottomText] = useState("")
+    const [randomImage, setRandomImage] = useState("http://i.imgflip.com/1bij.jpg")
+    const [allMemeImages, setAllMemeImages] = useState([])
 
-    componentDidMount(){
+    useEffect(() =>{
         fetch("https://api.imgflip.com/get_memes")
             .then(response => response.json())
-            .then(response => {                 
-                const { memes } = response.data                
-                this.setState({allMemeImages: memes})
-            })
-    }
+            .then(response => {
+                const {memes} = response.data
+                setAllMemeImages(memes)
+            })        
+    }, [])
 
-    handleChange = (event) => {
-        const {name, value} = event.target
-        this.setState({ [name]: value })
-
-        console.log(this.state.allMemeImages)
-    }
-
-    handleSubmit = (event) => {
+    function handleSubmit(event){
         event.preventDefault()
-        const randomNumber = Math.floor(Math.random() * this.state.allMemeImages.length)
-        const urlRandomImage = this.state.allMemeImages[randomNumber].url        
-        this.setState({randomImage: urlRandomImage})
+        const randomNumber = Math.floor(Math.random() * allMemeImages.length)
+        const urlRandomImage = allMemeImages[randomNumber].url        
+        setRandomImage(urlRandomImage)
     }
 
-    render() {
-        return(
-            <MemeComponent handleChange={this.handleChange} handleSubmit={this.handleSubmit} data={this.state}/>
-        )
-    }
+    return(
+        <main>
+                <form onSubmit={handleSubmit}>    
+                    <input 
+                        name="topText"
+                        value={topText}
+                        type="text"
+                        onChange={e => setTopText(e.target.value)}
+                    />
+
+                    <input 
+                        name="bottomText"
+                        value={bottomText}
+                        type="text"
+                        onChange={e => setBottomText(e.target.value)}
+                     />
+
+                    <button>Generate</button>
+
+                    <div className="meme">
+                        <img src={randomImage} alt=""/>
+                        <h2 className="top">{topText}</h2>
+                        <h2 className="bottom">{bottomText}</h2>
+                    
+                    </div>
+                </form>
+            </main>
+    )
 }
 
 export default MemeGenerator
