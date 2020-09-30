76710fcdd2bb09a5d95305c1f51d1bc10345e7f9
heliorrfreitas <heliorui.ramosfreitas@gus.pittstate.edu>
2020-09-29 22:23:33 -0500

    Updates

---
+++ precache-manifest.44b8019701bc9bd55c1f3c6ec782e41e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "439d03194d1bf8a44bc2b45c873d71aa",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "2bcf1480d56faca29333",
+    "url": "/meme-generator/static/css/main.724808c0.chunk.css"
+  },
+  {
+    "revision": "efab2afd35ba014e42b2",
+    "url": "/meme-generator/static/js/2.346b0a74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meme-generator/static/js/2.346b0a74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bcf1480d56faca29333",
+    "url": "/meme-generator/static/js/main.29bece2a.chunk.js"
+  },
+  {
+    "revision": "163fc7ea317d65653242",
+    "url": "/meme-generator/static/js/runtime-main.66d5941a.js"
+  }
+]);
